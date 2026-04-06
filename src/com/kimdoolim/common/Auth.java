68229d0c69d9a142e42f6d49645f3b1df055cf3b
solmin <solmin614@naver.com>
2026-04-06 19:57:57 +0900

    <sm> 로그인 관련처리

--- src/com/kimdoolim/common/Auth.java
+++ src/com/kimdoolim/common/Auth.java
@@ -1,8 +1,12 @@
 package com.kimdoolim.common;
 
+import com.kimdoolim.dto.Permission;
 import com.kimdoolim.dto.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -22,15 +26,58 @@ public class Auth {
         한계정으로 중복로그인 시도시 원래 로그인되어있던 프로그램에서 로그아웃 시켜버리고
         현재 로그인시도중인 프로세스에서 로그인되도록..
     */
-    public static int login(String memberId, String memberPwd) {
-        Connection conn = MySql.getMySql().getConnection();
+    public static int login(String userId, String userPwd) {
+        DataBase db = MySql.getMySql();
+        Connection conn = db.getConnection();
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "select * from User where id = ? and password = ?";
 
+        User user = null;
+        int result = 0;
 
-        threadLocalUserId.set(1);
-        return 0;
+        try {
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, userId);
+            pstmt.setString(2, userPwd);
+
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                user = User.builder()
+                        .userId(rs.getInt("USER_ID"))
+                        .schoolId(rs.getInt("SCHOOL_ID"))
+                        .id(rs.getString("ID"))
+                        .password(rs.getString("PASSWORD"))
+                        .permission(Permission.valueOf(rs.getString("PERMISSION")))
+                        .name(rs.getString("NAME"))
+                        .phone(rs.getString("PHONE"))
+                        .gradeNo(rs.getInt("GRADE_NO"))
+                        .classNo(rs.getInt("CLASS_NO"))
+                        .isActive(Boolean.parseBoolean(rs.getString("IS_ACTIVE")))
+                        .build();
+            }
+
+            if(user != null) {
+                result = 1;
+                loginUser.put(user.getUserId(), user);
+                threadLocalUserId.set(user.getUserId());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            db.close(rs);
+            db.close(pstmt);
+            db.close(conn);
+        }
+        System.out.println("loginUser : " + user);
+        return result;
     }
 
-    public static User getMemberInfo() {
+    // 로그인한 사용자 정보 가져오는 메소드입니다
+    //ex) Auth.getUserInfo() return User
+    public static User getUserInfo() {
         Integer id = threadLocalUserId.get();
 
         if (id == null) return null;
