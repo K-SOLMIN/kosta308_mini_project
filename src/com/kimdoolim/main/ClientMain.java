0e1b667640cee25a8ef67afe273baadb655496ac
solmin <solmin614@naver.com>
2026-04-06 20:16:28 +0900

    <sm> 권한에따른 화면 분기

--- src/com/kimdoolim/main/ClientMain.java
+++ src/com/kimdoolim/main/ClientMain.java
@@ -1,4 +1,20 @@
 package com.kimdoolim.main;
 
+import com.kimdoolim.common.Auth;
+import com.kimdoolim.dto.Permission;
+import com.kimdoolim.dto.User;
+import com.kimdoolim.main.view.LoginView;
+import com.kimdoolim.main.view.MainView;
+
 public class ClientMain {
+    private static User loginUser = null;
+
+    public static void main(String[] args) {
+        new LoginView().loginView();
+
+        MainView mainView = new MainView();
+
+        if(loginUser.getPermission() == Permission.USER) mainView.userMainView();
+        else mainView.managerMainView();
+    }
 }
