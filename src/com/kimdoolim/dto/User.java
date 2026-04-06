d6ce1742c36df10fb4d53a7042a435fa335634de
solmin <solmin614@naver.com>
2026-04-06 19:58:11 +0900

    <sm> user클래스 builder패턴 적용

--- src/com/kimdoolim/dto/User.java
+++ src/com/kimdoolim/dto/User.java
@@ -14,106 +14,103 @@ public class User {
     private int classNo;
     private boolean isActive;
 
-    public User() { }
+    private User() { }
 
-    public User(int userId, int schoolId, String id, String password, Permission permission, String name, String phone, int gradeNo, int classNo, boolean isActive) {
-        this.userId = userId;
-        this.schoolId = schoolId;
-        this.id = id;
-        this.password = password;
-        this.permission = permission;
-        this.name = name;
-        this.phone = phone;
-        this.gradeNo = gradeNo;
-        this.classNo = classNo;
-        this.isActive = isActive;
+    public static Builder builder() {
+        return new Builder();
     }
 
-    public int getUserId() {
-        return userId;
+    public static class Builder {
+        private int userId;
+        private int schoolId;
+        private String id;
+        private String password;
+        private Permission permission;
+        private String name;
+        private String phone;
+        private int gradeNo;
+        private int classNo;
+        private boolean isActive;
+
+        public Builder userId(int userId) {
+            this.userId = userId;
+            return this;
+        }
+        public Builder schoolId(int schoolId) {
+            this.schoolId = schoolId;
+            return this;
+        }
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+        public Builder permission(Permission permission) {
+            this.permission = permission;
+            return this;
+        }
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+        public Builder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+        public Builder gradeNo(int gradeNo) {
+            this.gradeNo = gradeNo;
+            return this;
+        }
+        public Builder classNo(int classNo) {
+            this.classNo = classNo;
+            return this;
+        }
+        public Builder isActive(boolean isActive) {
+            this.isActive = isActive;
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+            user.userId     = this.userId;
+            user.schoolId   = this.schoolId;
+            user.id         = this.id;
+            user.password   = this.password;
+            user.permission = this.permission;
+            user.name       = this.name;
+            user.phone      = this.phone;
+            user.gradeNo    = this.gradeNo;
+            user.classNo    = this.classNo;
+            user.isActive   = this.isActive;
+            return user;
+        }
     }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public int getSchoolId() {
-        return schoolId;
-    }
-
-    public void setSchoolId(int schoolId) {
-        this.schoolId = schoolId;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Permission getPermission() {
-        return permission;
-    }
-
-    public void setPermission(Permission permission) {
-        this.permission = permission;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public int getGradeNo() {
-        return gradeNo;
-    }
-
-    public void setGradeNo(int gradeNo) {
-        this.gradeNo = gradeNo;
-    }
-
-    public int getClassNo() {
-        return classNo;
-    }
-
-    public void setClassNo(int classNo) {
-        this.classNo = classNo;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
+    // getter
+    public int getUserId()             { return userId; }
+    public int getSchoolId()           { return schoolId; }
+    public String getId()              { return id; }
+    public String getPassword()        { return password; }
+    public Permission getPermission()  { return permission; }
+    public String getName()            { return name; }
+    public String getPhone()           { return phone; }
+    public int getGradeNo()            { return gradeNo; }
+    public int getClassNo()            { return classNo; }
+    public boolean isActive()          { return isActive; }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
         return userId == user.userId && schoolId == user.schoolId && gradeNo == user.gradeNo && classNo == user.classNo && isActive == user.isActive && Objects.equals(id, user.id) && Objects.equals(password, user.password) && permission == user.permission && Objects.equals(name, user.name) && Objects.equals(phone, user.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, schoolId, id, password, permission, name, phone, gradeNo, classNo, isActive);
     }
 
     @Override
@@ -130,10 +127,5 @@ public class User {
                 ", classNo=" + classNo +
                 ", isActive=" + isActive +
                 '}';
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(userId, schoolId, id, password, permission, name, phone, gradeNo, classNo, isActive);
     }
 }
