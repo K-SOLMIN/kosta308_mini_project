b4a0e0ec4cc4e07a26127912cb219b0d5620e779
solmin <solmin614@naver.com>
2026-04-06 20:24:46 +0900

    <sm> main에서 로그인성공시 자신의 userId값을 serversocket으로 보내는 기능 구현

--- src/com/kimdoolim/main/ClientMain.java
+++ src/com/kimdoolim/main/ClientMain.java
@@ -6,11 +6,43 @@ import com.kimdoolim.dto.User;
 import com.kimdoolim.main.view.LoginView;
 import com.kimdoolim.main.view.MainView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import static java.lang.System.out;
+
 public class ClientMain {
-    private static User loginUser = null;
 
     public static void main(String[] args) {
         new LoginView().loginView();
+        User loginUser = Auth.getUserInfo();
+
+//        try {
+//            Socket socket = new Socket("localhost", 8080);
+//            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+//
+//            System.out.println("loginUser : " + loginUser);
+//
+//            out.println(loginUser.getId());
+//
+//            new Thread(() -> {
+//                try {
+//                    BufferedReader msgReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                    String alarmMsg = "";
+//
+//                    while ((alarmMsg = msgReader.readLine()) != null) {
+//                        out.println(alarmMsg);
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }).start();
+//        } catch(IOException e) {
+//            e.printStackTrace();
+//        }
 
         MainView mainView = new MainView();
 
