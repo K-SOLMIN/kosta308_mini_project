0316cfcf44da19984fc1b1a5b721664b4dac61e2
solmin <solmin614@naver.com>
2026-04-06 19:57:39 +0900

    <sm> 로그인 화면 구현

--- src/com/kimdoolim/main/view/LoginView.java
+++ src/com/kimdoolim/main/view/LoginView.java
@@ -1,26 +1,29 @@
 package com.kimdoolim.main.view;
 
+import com.kimdoolim.common.Auth;
+
 import java.util.Scanner;
 
 public class LoginView {
     public void loginView() {
         Scanner scanner = new Scanner(System.in);
+        int result = 0;
 
         System.out.println("=============================");
-        System.out.println("         로그인              ");
+        System.out.println("         로그인 화면           ");
         System.out.println("=============================");
 
-        System.out.print("아이디: ");
-        String userId = scanner.nextLine();
+        while(true) {
+            System.out.print("아이디: ");
+            String userId = scanner.nextLine();
 
-        System.out.print("비밀번호: ");
-        String password = scanner.nextLine();
+            System.out.print("비밀번호: ");
+            String password = scanner.nextLine();
 
-        System.out.println("=============================");
-        System.out.println("입력된 아이디: " + userId);
-        System.out.println("입력된 비밀번호: " + password);
-        System.out.println("=============================");
+            result = Auth.login(userId, password);
 
-        scanner.close();
+            if(result == 1) break;
+            else System.out.println("회원정보가 일치하지 않습니다.");
+        }
     }
 }
