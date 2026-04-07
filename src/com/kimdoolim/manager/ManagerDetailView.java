89157ee7a22f9153aa5cc9a1320406870f61d5e8
solmin <solmin614@naver.com>
2026-04-07 10:22:43 +0900

    <sm> 최상위 관리자 메뉴 구현중

--- src/com/kimdoolim/manager/ManagerDetailView.java
+++ src/com/kimdoolim/manager/ManagerDetailView.java
@@ -3,6 +3,8 @@ package com.kimdoolim.manager;
 import java.util.Scanner;
 
 public class ManagerDetailView {
+    FacilityManageView facilityManageView = new FacilityManageView();
+
     public void topManagerView() {
         Scanner scanner = new Scanner(System.in);
 
@@ -10,10 +12,11 @@ public class ManagerDetailView {
             System.out.println("=============================");
             System.out.println("      최상위 관리자 메뉴      ");
             System.out.println("=============================");
-            System.out.println(" 1. 시설 / 비품 관리");
-            System.out.println(" 2. 사용자 등록");
-            System.out.println(" 3. 예약 요청 조회");
-            System.out.println(" 4. 제한 시간 관리");
+            System.out.println(" 1. 시설 관리");
+            System.out.println(" 2. 비품 관리");
+            System.out.println(" 3. 사용자 등록");
+            System.out.println(" 4. 예약 요청 조회");
+            System.out.println(" 5. 제한 시간 관리");
             System.out.println(" 0. 뒤로 가기");
             System.out.println("=============================");
             System.out.print("메뉴 선택 : ");
@@ -22,20 +25,24 @@ public class ManagerDetailView {
 
             switch (choice) {
                 case 1:
-                    System.out.println(">> 시설 / 비품 관리로 이동합니다.");
-
+                    System.out.println(">> 시설 관리로 이동합니다.");
+                    facilityManageView.facilityManageView();
                     break;
                 case 2:
-                    System.out.println(">> 사용자 등록으로 이동합니다.");
-
+                    System.out.println(">> 비품 관리로 이동합니다.");
+                    //equipmentView();
                     break;
                 case 3:
-                    System.out.println(">> 예약 요청 조회로 이동합니다.");
-
+                    System.out.println(">> 사용자 등록으로 이동합니다.");
+                    //userRegisterView();
                     break;
                 case 4:
+                    System.out.println(">> 예약 요청 조회로 이동합니다.");
+                    //reservationRequestView();
+                    break;
+                case 5:
                     System.out.println(">> 제한 시간 관리로 이동합니다.");
-
+                    //timeLimitView();
                     break;
                 case 0:
                     System.out.println(">> 이전 메뉴로 돌아갑니다.");
@@ -45,4 +52,5 @@ public class ManagerDetailView {
             }
         }
     }
+
 }
