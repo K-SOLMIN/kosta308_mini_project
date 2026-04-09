5073dfbc96d023caa0d8a148c06ef10ab06cbb91
solmin <solmin614@naver.com>
2026-04-09 15:19:13 +0900

    <sm> 코드정리

--- src/com/kimdoolim/main/ServerMain.java
+++ src/com/kimdoolim/main/ServerMain.java
@@ -15,16 +15,16 @@ public class ServerMain {
         AlarmScheduler alarmScheduler = AlarmScheduler.getAlarmScheduler();
         alarmScheduler.startScheduleTest(); // 스케줄러테스트 시작
 
-            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-                System.out.println("🚀 [알림 서버] 포트 " + PORT + "에서 대기 중...");
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("🚀 [알림 서버] 포트 " + PORT + "에서 대기 중...");
 
-                while (true) {
-                    Socket socket = serverSocket.accept();
-                    // 클라이언트 접속 시 전담 스레드 생성
-                    new Thread(new SocketSession(socket)).start();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            while (true) {
+                Socket socket = serverSocket.accept();
+                // 클라이언트 접속 시 전담 스레드 생성
+                new Thread(new SocketSession(socket)).start();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 }
