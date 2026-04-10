adbfeafd5d4c4c1c77e19b625a353a5513195af8
solmin <solmin614@naver.com>
2026-04-10 12:11:28 +0900

    <sm> docker 파일 작성중

--- src/com/kimdoolim/common/MySql.java
+++ src/com/kimdoolim/common/MySql.java
@@ -6,9 +6,11 @@ import java.sql.SQLException;
 
 public class MySql implements Database {
 
-    // 한글 깨짐 방지를 위해 UTF-8 인코딩 설정 추가
-    private static final String URL = "jdbc:mysql://localhost:3307/kimdoolim?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Seoul";
+    // 한글 깨짐 방지를 위해 UTF-8 인코딩 설정 추가(local용)
+//    private static final String URL = "jdbc:mysql://localhost:3307/kimdoolim?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Seoul";
 
+    //docker용
+    private static final String URL = "jdbc:mysql://mysql:3306/kimdoolim?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Seoul";
     private static final String USER     = "root";
     private static final String PASSWORD = "1111";
 
